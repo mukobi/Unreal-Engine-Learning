7c3a4852f73d8d0c9af88b2f68c0406f3fcdc11e
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-06-12 12:21:38 -0700

    Start Learn Unreal Engine C++ In One Hour project

---
+++ Tutorial Projects/01 Learn Unreal Engine C++ In One Hour/UnrealCppInOneHour/Source/UnrealCppInOneHour/UnrealCppInOneHour.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealCppInOneHour.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCppInOneHour, "UnrealCppInOneHour" );
+ 
